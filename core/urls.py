7e084a5db3a1a19a743d6986a3504a310dff5b0f
steve-ongera <gadafisteve001@gmail.com>
2024-11-18 01:03:23 +0300

    modified the url for login

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('withdraw/', views.withdraw, name='withdraw'),
     path('send_money/', views.send_money, name='send_money'),
     path('register/', views.register, name='register'), # Register view
-    path('login/', views.login_view, name='login'),     # Login view
+    path('login/', views.custom_login, name='login'),     # Login view
     path('logout/', views.custom_logout, name='logout') # Logout view
 ]
